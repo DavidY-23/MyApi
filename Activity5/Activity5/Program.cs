97238e2983e21dc4bbfcbe248d786d297ceb273a
David <55970839+DavidY-23@users.noreply.github.com>
2022-10-03 18:48:25 -0400

    Add files via upload

---
+++ Activity5/Activity5/Program.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+
+namespace Activity5
+{
+    class Joke
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("setup")]
+        public string Setup { get; set; }
+
+        [JsonProperty("punchline")]
+        public string Punchline { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+    }
+
+    public class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            await ProcessRepositories();
+        }
+        private static async Task ProcessRepositories()
+        {
+
+            while(true)
+            {
+                Console.WriteLine("Type joke for a random joke!");
+                var readJoke = Console.ReadLine();
+                if (string.IsNullOrEmpty(readJoke))
+                {
+                    break;
+                }
+                try
+                {
+                    var result = await client.GetAsync("https://official-joke-api.appspot.com/random_joke");
+                    var resultRead = await result.Content.ReadAsStringAsync();
+                    var joke = JsonConvert.DeserializeObject<Joke>(resultRead);
+
+                    Console.WriteLine(joke.Setup);
+                    Console.WriteLine(joke.Punchline);
+                    Console.WriteLine("This joke was number: " + joke.Id + " and the type of joke is " + joke.Type);
+                }
+                catch (Exception)
+                {
+                    Console.Write("Error");
+                }
+            }
+        }
+    }
+
+}
