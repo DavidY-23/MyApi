d359ece477fecec1bcc295cf576d66220c3d5298
David <55970839+DavidY-23@users.noreply.github.com>
2022-10-03 18:47:28 -0400

    Add files via upload

---
+++ WebApiClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            await ProcessRepositories();
+        }
+        private static async Task ProcessRepositories()
+        {
+            while (true)
+            try
+            {
+                Console.WriteLine("Enter Pokemon.");
+                var pokemonName = console.ReadLine();
+
+                if (string.IsNullOrEmpty(pokemonName))
+                {
+                    break;
+                }
+
+                var result = await client.GetAsync("https://pokeapi.co/api/v2/pokemon" + pokemonName.ToLower());
+                var resultRead = await result.Content.ReadAsStringAsync();
+            }
+            catch (Exception)
+            {
+                console.WriteLine("Error");
+            }
+        }
+
+    }
+}
